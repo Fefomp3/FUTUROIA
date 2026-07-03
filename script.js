933d617256bc82b0c65932c07f022d19436f916e
Fefomp3 <felipe.tozzini.teixeira@escola.pr.gov.br>
2026-07-03 11:28:07 +0000

    htrhhrthr

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
 const perguntas = [
   {
     enunciado: "IA PODE DESENVOLVER EMOÇÕES??",
@@ -12,4 +13,26 @@ const perguntas = [
     enunciado: "UMA IA TE OFEREÇE UM SORVETE. VOCÊ ACEITA??",
     alternativas: ["SIM", "NÃO"],
   },
+  {
+    enunciado: "pergunta 3",
+    alternativas: ["alt1", "alt2"]
+  },
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+};
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+  }
+};
+
+mostraPergunta();
+mostraAlternativas();
